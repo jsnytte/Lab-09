550423c20620b0fcceb78f5688b3d943659a5f2e
jsnytte <109549086+jsnytte@users.noreply.github.com>
2024-04-13 21:45:08 -0400

    main file commit

---
+++ Lab9.py
@@ -0,0 +1,40 @@
+class Encode():
+    def __init__(self, str):
+        self.str = str
+        self.num_list = []
+        for i in self.str:
+            self.num_list.append(int(i))
+
+    def encode(self):
+        encoded_str = ""
+        for i in self.num_list:
+            digit = i + 3
+            if digit > 9:
+                digit = digit - 10
+            encoded_str += str(digit)
+        self.str = encoded_str
+        
+class Decode():
+    def __init__(self, str):
+        #init
+        pass
+    
+    def decode(self):
+        #decode
+        pass
+
+while True:
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+    option = int(input("Please enter an option: "))
+
+    if option == 1:
+        password = input("Please enter your password to encode: ")
+        encoder = Encode(password)
+        encoder.encode()
+        password = encoder.str
+        print("Your password has been encoded and stored!\n")
+    elif option == 2:
+       # decode
+       pass
+    elif option == 3:
+        break
